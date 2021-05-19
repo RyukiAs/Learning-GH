af75ed74a9bb044d27df85e03401ac513b69037a
Yuki <shionscars@gmail.com>
2021-05-18 21:57:07 -0500

    Initial Commit

---
+++ learningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learningGH
+//
+//  Created by Ben Johnson on 5/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ learningGH/learningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHApp.swift
+//  learningGH
+//
+//  Created by Ben Johnson on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct learningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
